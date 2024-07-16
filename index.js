fb1c3c91376b6162a15ffe0637bd84f8ce0b2554
AdibHoque <adibhoque04@gmail.com>
2024-07-16 17:29:10 +0600

    Connected to MongoDb and got the server running

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('hi') 
+require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+const app = express()
+const port = process.env.PORT || 5000
+const jwt = require('jsonwebtoken');
+
+app.use(cors())
+app.use(express.json())
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.knvnnno.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("API server is running for TakaPay!")
+})
+
+app.listen(port, () => {
+  console.log("Listening to Port: ", port)
+})
